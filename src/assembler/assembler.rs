90c6f24c6c78d10327fae63b10a3bb06e6c63956
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2019-10-08 19:34:51 +0800

    some framework

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -1,1 +1,19 @@
-pub struct Assembler {}
+use crate::assembler::assembler_instruction::AssemblerInstruction;
+
+pub struct Assembler {
+    instructions: Vec<AssemblerInstruction>
+}
+
+impl Assembler {
+    pub fn new(instructions: Vec<AssemblerInstruction>) -> Assembler {
+        Assembler {
+            instructions
+        }
+    }
+
+    pub fn process(&self) -> Result<Vec<u8>, &'static str> {}
+
+    pub fn process_first_phase(&self) {}
+
+    pub fn process_second_phase(&self) {}
+}
--- src/assembler/elf.rs
+++ src/assembler/elf.rs
@@ -1,3 +1,47 @@
-struct DELFHeader {
-    header_size: usize,
+pub struct DELFHeader {
+    identify: u32,
+    version: u32,
+    entry: u32,
+    program_header_offset: u32,
+    segment_header_offset: u32,
+    header_size: u32,
+    program_header_length: u32,
+    segment_header_length: u32,
+    segment_str_table_index: u32,
 }
+
+impl DELFHeader {
+    fn new() -> DELFHeader {
+        DELFHeader {
+            identify: 0x64656c66,//delf
+            version: 0,
+            entry: 0,
+            program_header_offset: 0,
+            segment_header_offset: 0,
+            header_size: 64,
+            program_header_length: 0,
+            segment_header_length: 0,
+            segment_str_table_index: 0,
+        }
+    }
+
+    fn encode_to_bytes(&self) -> Vec<u8> {
+        return Vec::new();
+    }
+
+    fn decode_from_bytes(headers: Vec<u8>) -> DELFHeader {
+         DELFHeader{
+             identify: 0,
+             version: 0,
+             entry: 0,
+             program_header_offset: 0,
+             segment_header_offset: 0,
+             header_size: 0,
+             program_header_length: 0,
+             segment_header_length: 0,
+             segment_str_table_index: 0
+         }
+    }
+}
+
+
